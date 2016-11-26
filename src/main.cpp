019e823dfaccc09e3f6b2ff04b0e91e60004a747
diegomarq <diegomarques.azevedo@gmail.com>
2016-11-26 18:38:08 -0200

    inicia processador

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include "defines.h"
+#include <unistd.h>
 
+// Instanciado uma vez variavel extern
 vector<proc_t> vet_processos;
 vector<int> vet_memoria;
-
 queue<proc_t> fila_prioridade_zero;
+
+#define CLOCK 100000
 
 int main(int argc, char **argv) {
 
@@ -17,17 +20,29 @@ int main(int argc, char **argv) {
 		arq.assign(argv[1]);
 	}
 
-	// carrega vetor de processos
+	// Carrega vetor de processos
 	UTILS::carregaProcesso(arq.data());
 
-	// carrega vetor de memoria
+	// Carrega vetor de memoria
 	UTILS::inicializaMemoria();
 
 	// carrega processos na fila de prioridade um
 	UTILS::carregaFilaPrioridadeZero();
 
+	// Inicia processador
+	while(1) {
+		
+		if(vet_processos.size() > 0) {
+			// reserva memoria
+			
+		}
+
+		usleep(CLOCK);
+		break;
+	}
+
 	//DEBUG::mostrarProcesso(vet_processos[0]);
-	DEBUG::mostrarFilaPrioridadeZero();
+//	DEBUG::mostrarFilaPrioridadeZero();
 
 	return 0;
 }
